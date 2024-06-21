1f9fb228c8d6daaa4fe1e584c1ebbc54119f15c9
Dogan Robert Stephano <96503610+chiefdogan@users.noreply.github.com>
2024-06-21 16:38:31 +0300

    Delete group16-data_structure_and_algorithms directory

--- group16-data_structure_and_algorithms/5-sum_even.py
+++
@@ -1,6 +0,0 @@
-# 5-sum_even.py
-def sum_even_numbers(my_list):
-    return sum(num for num in my_list if num % 2 == 0)
-
-if __name__ == "__main__":
-    print(sum_even_numbers([1, 2, 3, 4, 5, 6]))  # Output: 12
